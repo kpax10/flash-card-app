26fc37d97945890f79d2ed2dfb166d2e5524a779
Kyle <notxap55@yahoo.com>
2024-09-11 15:53:23 -0700

    cvs file implemented random word generates on card

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from tkinter import *
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+def new_random_word():
+    df = pd.read_csv('data/italian_words.csv')
+    random_word = df.sample(n=1)
+    italian_word = random_word.iloc[0]['Italian']
+    canvas.itemconfig(word_canvas, text=italian_word)
 
 
 window = Tk()
@@ -12,16 +18,17 @@ window.resizable(False, False)
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Language", font=('Ariel', 40, 'italic'))
-canvas.create_text(400, 263, text="word", font=('Ariel', 60, 'bold'))
+language_canvas = canvas.create_text(400, 150, text="Language", font=('Ariel', 40, 'italic'))
+word_canvas = canvas.create_text(400, 263, text="word", font=('Ariel', 60, 'bold'))
 canvas.grid(column=0, columnspan=2, row=0)
 
 unknown_img = PhotoImage(file='images/wrong.png')
-unknown_button = Button(image=unknown_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR)
+unknown_button = Button(image=unknown_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=new_random_word)
 unknown_button.grid(column=0, row=1)
 known_img = PhotoImage(file='images/right.png')
-known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR)
+known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=new_random_word)
 known_button.grid(column=1, row=1)
 
+print(type(word_canvas))
 
 window.mainloop()
