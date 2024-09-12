ab18215a4cb87a8f18d9b732bed1cbfeb4e9d311
Kyle <notxap55@yahoo.com>
2024-09-12 14:50:35 -0700

    card flip timer bugfix

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
 
 def next_card():
+    global current_card, timer
+    window.after_cancel(timer)
     current_card = random.choice(to_learn_list)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card['Italian'])
+    canvas.itemconfig(card_title, text="French", fill='black')
+    canvas.itemconfig(card_word, text=current_card['Italian'], fill='black')
+    canvas.itemconfig(canvas_image, image=card_front_img)
+    timer = window.after(3000, flip_card)
 
-    def flip_card():
-        canvas.itemconfig(canvas_image, image=card_back_img)
-        canvas.itemconfig(card_title, fill='white')
-        canvas.itemconfig(card_title, text='English')
-        canvas.itemconfig(card_word, fill='white')
-        canvas.itemconfig(card_word, text=current_card['English'])
-        window.after_cancel(task)
 
-    task = window.after(3000, flip_card)
+def flip_card():
+    canvas.itemconfig(canvas_image, image=card_back_img)
+    canvas.itemconfig(card_title, fill='white')
+    canvas.itemconfig(card_title, text='English')
+    canvas.itemconfig(card_word, fill='white')
+    canvas.itemconfig(card_word, text=current_card['English'])
 
 
 df = pd.read_csv('data/italian_words.csv')
@@ -28,6 +31,8 @@ to_learn_list = df.to_dict(orient='records')
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.title('Flash Cards')
+timer = window.after(3000, flip_card)
+
 # window.resizable(False, False)
 
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -39,10 +44,10 @@ card_word = canvas.create_text(400, 263, text="word", font=('Ariel', 60, 'bold')
 canvas.grid(column=0, columnspan=2, row=0)
 
 unknown_img = PhotoImage(file='images/wrong.png')
-unknown_button = Button(image=unknown_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR)
+unknown_button = Button(image=unknown_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=next_card)
 unknown_button.grid(column=0, row=1)
 known_img = PhotoImage(file='images/right.png')
-known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR)
+known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=next_card)
 known_button.grid(column=1, row=1)
 
 next_card()
