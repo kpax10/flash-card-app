2655cf5c4aafea6e1a5a07f71b33582d1731e157
Kyle <notxap55@yahoo.com>
2024-09-12 16:05:15 -0700

    completed

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from gettext import textdomain
+import csv
 from tkinter import *
 import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
+
+
+def remove_word():
+    if current_card in to_learn_list:
+        to_learn_list.remove(current_card)
+        print(f'{current_card} removed')
+        print(len(to_learn_list))
+
+        with open('data/words_to_learn.csv', 'w', newline='') as file:
+            header = to_learn_list[0].keys()
+            writer = csv.DictWriter(file, fieldnames=header)
+            writer.writeheader()
+            writer.writerows(to_learn_list)
+    next_card()
 
 
 def next_card():
@@ -25,15 +39,17 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card['English'])
 
 
-df = pd.read_csv('data/italian_words.csv')
-to_learn_list = df.to_dict(orient='records')
+try:
+    df = pd.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    df = pd.read_csv('data/italian_words.csv')
+finally:
+    to_learn_list = df.to_dict(orient='records')
 
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.title('Flash Cards')
 timer = window.after(3000, flip_card)
-
-# window.resizable(False, False)
 
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file='images/card_front.png')
@@ -47,7 +63,7 @@ unknown_img = PhotoImage(file='images/wrong.png')
 unknown_button = Button(image=unknown_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=next_card)
 unknown_button.grid(column=0, row=1)
 known_img = PhotoImage(file='images/right.png')
-known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=next_card)
+known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=remove_word)
 known_button.grid(column=1, row=1)
 
 next_card()
