14bca4b2f931b106203f4c1e5ebe8a5b1a95b2a8
Kyle <notxap55@yahoo.com>
2024-09-12 14:30:34 -0700

    card flipping properly

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
+from gettext import textdomain
 from tkinter import *
 import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-def new_random_word():
-    df = pd.read_csv('data/italian_words.csv')
-    random_word = df.sample(n=1)
-    italian_word = random_word.iloc[0]['Italian']
-    canvas.itemconfig(word_canvas, text=italian_word)
 
+def next_card():
+    current_card = random.choice(to_learn_list)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card['Italian'])
+
+    def flip_card():
+        canvas.itemconfig(canvas_image, image=card_back_img)
+        canvas.itemconfig(card_title, fill='white')
+        canvas.itemconfig(card_title, text='English')
+        canvas.itemconfig(card_word, fill='white')
+        canvas.itemconfig(card_word, text=current_card['English'])
+        window.after_cancel(task)
+
+    task = window.after(3000, flip_card)
+
+
+df = pd.read_csv('data/italian_words.csv')
+to_learn_list = df.to_dict(orient='records')
 
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.title('Flash Cards')
-window.resizable(False, False)
+# window.resizable(False, False)
 
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file='images/card_front.png')
-canvas.create_image(400, 263, image=card_front_img)
-language_canvas = canvas.create_text(400, 150, text="Language", font=('Ariel', 40, 'italic'))
-word_canvas = canvas.create_text(400, 263, text="word", font=('Ariel', 60, 'bold'))
+card_back_img = PhotoImage(file='images/card_back.png')
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="Language", font=('Ariel', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text="word", font=('Ariel', 60, 'bold'))
 canvas.grid(column=0, columnspan=2, row=0)
 
 unknown_img = PhotoImage(file='images/wrong.png')
-unknown_button = Button(image=unknown_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=new_random_word)
+unknown_button = Button(image=unknown_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR)
 unknown_button.grid(column=0, row=1)
 known_img = PhotoImage(file='images/right.png')
-known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR, command=new_random_word)
+known_button = Button(image=known_img, borderwidth=0, relief='flat', bg=BACKGROUND_COLOR)
 known_button.grid(column=1, row=1)
 
-print(type(word_canvas))
+next_card()
+
 
 window.mainloop()
